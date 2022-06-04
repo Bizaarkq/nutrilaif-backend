a2763f652d586983a5c49a99d9096b058f49ffd1
Bizaarkq <isaac.bernab.01@gmail.com>
2022-06-04 17:58:33 -0600

    🔥 organizando archivos, response custom

---
+++ app/Helpers/Respuesta.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers;
+
+class Respuesta
+{
+    const titulo_exito_generico = 'Exito';
+    const mensaje_exito_generico = 'Se pudo realizar la acción';
+
+    const titulo_error_generico = 'Error';
+    const mensaje_error_generico = 'No se pudo realizar la accion, intente nuevamente';
+}
