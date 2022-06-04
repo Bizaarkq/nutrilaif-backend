a493e2d3f683fa5c14326237042b43357f2a1a33
otrodesconocidomas <rv17012@ues.edu.sv>
2022-06-04 14:39:21 -0600

    ✨ alimentos

---
+++ app/Models/alimento/Alimento.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\alimento;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Alimento extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table='nutrilaif.alimentos';
+    protected $fillable=[
+        'nombre_alimento',
+        'calorias_alimento',
+        'grasas_alimento',
+        'proteinas_alimento',
+        'carbohidratos_alimento',
+        'hierro_alimento',
+        'potasio_alimento',
+        'calcio_alimento',
+        'sodio_alimento'
+    ];
+    protected $dates=['created_at','updated_at'];
+}
