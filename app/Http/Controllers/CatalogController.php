fb2152dd65c72e1ced6314168f76b6b5138070d1
otrodesconocidomas <rv17012@ues.edu.sv>
2022-08-11 22:12:05 -0600

    ✨ Tabla de frecuencia base

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\Catalogo\MenuInicio;
+use Arcanedev\LogViewer\Entities\Log as EntitiesLog;
 
 class CatalogController extends Controller
 {
@@ -21,6 +22,12 @@ class CatalogController extends Controller
 
     public function getMunicipios($id){
         return Departamento::find($id)->municipios;
+    }
+    public function getFrecuenciaBase(){
+        $listaBase=DB::table('nutri_catalog.alimentos_frecuencia_base')
+        ->select('nombre')->orderBy('id','asc')->get();
+        Log::warning(gettype($listaBase));
+        return $listaBase;
     }
 
 }
