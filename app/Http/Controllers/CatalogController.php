67e3775919d1d7c46c5e95a3f59252434f5336b5
otrodesconocidomas <rv17012@ues.edu.sv>
2022-08-12 20:25:35 -0600

    🔥 quitando los log

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\Catalogo\MenuInicio;
-use Arcanedev\LogViewer\Entities\Log as EntitiesLog;
 
 class CatalogController extends Controller
 {
@@ -26,7 +25,6 @@ class CatalogController extends Controller
     public function getFrecuenciaBase(){
         $listaBase=DB::table('nutri_catalog.alimentos_frecuencia_base')
         ->select('nombre')->orderBy('id','asc')->get();
-        Log::warning(gettype($listaBase));
         return $listaBase;
     }
 
