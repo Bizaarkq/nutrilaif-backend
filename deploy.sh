bf73c69fa12b2820e1e63ff43ca5e18b7fea29d4
bizaarkq <isaac.bernab.01@gmail.com>
2024-11-09 16:33:26 -0600

    🚀 auto realizar peticiones cada minuto para mantener vivo el servidor

--- deploy.sh
+++ deploy.sh
@@ -78,5 +78,14 @@ apachectl configtest || { echo "Error: Apache configuration failed."; exit 1; }
 
 echo "Configuration checks passed successfully."
 
+# do request to the server to check if it is up every one minute
+while true; do
+    curl -s https://nutrilaif-backend-573v.onrender.com > /dev/null
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 60
+done
+
 # Start Apache in the foreground
 apache2-foreground
